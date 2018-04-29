ed1ff05e70d5739e86edbe9f7a906cb2580c0902
Eric Bonnette <bonn9650@gmail.com>
2018-04-29 14:27:47 -0400

    Add place holder image for null or broken url

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -52,6 +52,7 @@ public class DetailActivity extends AppCompatActivity {
         populateUI();
         Picasso.with(this)
                 .load(sandwich.getImage())
+                .placeholder(R.drawable.sandwich_not_found)
                 .into(ingredientsIv);
 
         setTitle(sandwich.getMainName());
